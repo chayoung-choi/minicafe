8191bb7f7abb73d0b109883bf6fb2a47f72dd697
chayoung-choi <chayoung.dev@gmail.com>
2022-11-01 15:50:58 +0900

    feat: 프로젝트 환경설정 #1

---
+++ src/test/java/com/eden/minicafe/MinicafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden.minicafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicafeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
